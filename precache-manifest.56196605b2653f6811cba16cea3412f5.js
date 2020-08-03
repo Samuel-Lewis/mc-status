4d8dbf0732332dcc4ba0b9a40c169ad9e22b6168
Samuel Lewis <mazuel@gmail.com>
2020-08-03 17:26:51 +1000

    Updates

---
+++ precache-manifest.56196605b2653f6811cba16cea3412f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe47ec41b9095d053e8e3932b23ee1ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d165987d06a71a561755",
+    "url": "/static/css/main.a43e7795.chunk.css"
+  },
+  {
+    "revision": "5a030075a183b8adef80",
+    "url": "/static/js/2.6a7090bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7090bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d165987d06a71a561755",
+    "url": "/static/js/main.b5a8b642.chunk.js"
+  },
+  {
+    "revision": "8cfa48ddbeb0062ad082",
+    "url": "/static/js/runtime-main.94c53a5b.js"
+  }
+]);
