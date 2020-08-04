e17439d55a743f54d0f39509a6ef30e8dc90103d
Samuel Lewis <mazuel@gmail.com>
2020-08-04 22:18:44 +1000

    Updates

--- precache-manifest.7634c6cf8d54f04b9ddd0fa18085b8e1.js
+++ precache-manifest.7634c6cf8d54f04b9ddd0fa18085b8e1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e00dbd65a9f26d1045d6a6372e7262b5",
+    "revision": "76ac7455ed7b58fe95fb1fba87416ae4",
     "url": "/mc-status/index.html"
   },
   {
-    "revision": "b883e638cf305bf7ebae",
+    "revision": "86314b779c4b9267890c",
     "url": "/mc-status/static/css/2.9c8c8187.chunk.css"
   },
   {
-    "revision": "94d0dc249df1176244bd",
+    "revision": "cbda2e3553536c350d04",
     "url": "/mc-status/static/css/main.6ea0f23a.chunk.css"
   },
   {
-    "revision": "b883e638cf305bf7ebae",
-    "url": "/mc-status/static/js/2.27e25890.chunk.js"
+    "revision": "86314b779c4b9267890c",
+    "url": "/mc-status/static/js/2.54949188.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/mc-status/static/js/2.27e25890.chunk.js.LICENSE.txt"
+    "url": "/mc-status/static/js/2.54949188.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94d0dc249df1176244bd",
-    "url": "/mc-status/static/js/main.d0402f60.chunk.js"
+    "revision": "cbda2e3553536c350d04",
+    "url": "/mc-status/static/js/main.f45a91b8.chunk.js"
   },
   {
     "revision": "1130d1f50ed603a3f818",
